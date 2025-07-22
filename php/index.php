79a2e9c9d7be2b560e13d81a32c3f5b53a9f836a
sennaTia <1214998@student.roc-nijmegen.nl>
2025-07-22 11:43:45 +0200

    first push real commits
    pushing work

--- php/index.php
+++ php/index.php
@@ -4,8 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bibliotheek</title>
+       <link rel="stylesheet" href="style.css">
+
 </head>
 <body>
-    <div cla
+     <header>
+        <a href="reserveren.php" class="reserveren">Reserveren</a>
+    </header>
 </body>
 </html>
