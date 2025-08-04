dae20556e76012c48e61c0c8e391872c4488987c
sennaTia <1214998@student.roc-nijmegen.nl>
2025-08-04 13:38:22 +0200

    error met css
    vernoem naar css3 ipv normaal en toen error wg

--- php/reserveren.php
+++ php/reserveren.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <title>Boek Reserveren</title>
-  <link rel="stylesheet" href="../css/style.css">
+  <link rel="stylesheet" href="../css/style3.css">
 </head>
 <body>
   <header>
--- php/reserveringen.php
+++ php/reserveringen.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <title>Reserveringen</title>
-  <link rel="stylesheet" href="../css/style.css">
+  <link rel="stylesheet" href="../css/style3.css">
 </head>
 <body>
   <header>
